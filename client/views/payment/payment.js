671b069f5438aa383145f73b344e2c6f67dc8c4c
Anisuzzaman Khan <aniskhan001@outlook.com>
2017-09-27 21:12:48 +0600

    Instagrams update only on successful payment

--- client/views/payment/payment.js
+++ client/views/payment/payment.js
@@ -1,4 +1,4 @@
-var subscriptionEmail;
+let subscriptionEmail;
 
 Template.Payment.onCreated(function() {
 	
@@ -14,9 +14,9 @@ Template.Payment.onRendered(function() {
 		globalOnRendered();
 		$("input[autofocus]").focus();
 	});
-	var ttt = getPlanList()
+	let ttt = getPlanList()
 	.then(function(res) {
-		var i = 0;
+		let i = 0;
 		for(i=0;i<3;i++){
 			if(res[i].id) {
 				$('#plans').append($('<option>',{
@@ -38,23 +38,23 @@ Template.Payment.events({
 	'submit #register_form' : function(e, t) {
 		e.preventDefault();
 
-		var submit_button = $(t.find(":submit"));
+		let submit_button = $(t.find(":submit"));
 
 		// your account info
-		var register_username = t.find('#register_username').value.trim();
-		var register_email = t.find('#register_email').value.trim();
+		let register_username = t.find('#register_username').value.trim();
+		let register_email = t.find('#register_email').value.trim();
 		subscriptionEmail = register_email;
-		var register_phone = t.find('#register_phone').value.trim();
+		let register_phone = t.find('#register_phone').value.trim();
 
 		// Payment info
-		var plans = t.find('#plans').value.trim();
-		var setupfee = t.find('#setupfee').value.trim();
-		var cardnumber = t.find('#cardnumber').value.trim();
-		var expire_year = t.find('#expire_year').value.trim();
-		var expire_month = t.find('#expire_month').value.trim();
-		var cvv = t.find('#cvv').value.trim();  cvv = "293";
-		var country = t.find('#country').value.trim();
-		var zipcode = t.find('#zipcode').value.trim(); zipcode = "03-234";
+		let plans = t.find('#plans').value.trim();
+		let setupfee = t.find('#setupfee').value.trim();
+		let cardnumber = t.find('#cardnumber').value.trim();
+		let expire_year = t.find('#expire_year').value.trim();
+		let expire_month = t.find('#expire_month').value.trim();
+		let cvv = t.find('#cvv').value.trim();  cvv = "293";
+		let country = t.find('#country').value.trim();
+		let zipcode = t.find('#zipcode').value.trim(); zipcode = "03-234";
 
 		// email validation checking
 		if(!isValidEmail(register_email))
@@ -71,28 +71,9 @@ Template.Payment.events({
 			exp_year: expire_year,
 			address_country: country,
 			address_zip: zipcode
-			},stripeResponseHandler);
+		}, stripeResponseHandler);
 		
 		submit_button.button("loading");
-		var userId = Meteor.userId();
-
-		//const Instagrams =  new Mongo.Collection('instagrams');
-		var inst = Instagrams.find({belongs_to:Meteor.userId()}, {});
-		var instaId = Session.get("ig-id");
-		Instagrams.update({_id: instaId}, {
-		  $set: {
-			  active: true,
-			  paid : true
-		  }
-		  }, function (err, res) {
-			  if (err) {
-				  console.log(err);
-			  }
-			  if (res) {
-				  console.log(res);
-			  }
-		  });
-		sweetAlert("Setup fee and your Subscription is successfully charged!");
 	},
 
 });
@@ -106,40 +87,48 @@ function stripeResponseHandler(status, response) {
 	if (response.error) { 
 	  	console.log("stripe error");
 	} else {
-		var token = response.id;
-		console.log(token);
+		let token = response.id;
+		// console.log(token);
 
-		var iSetupFee = $('#setupfee').find(":selected").val();
-		var setupfee = 0.00;
+		let iSetupFee = $('#setupfee').find(":selected").val();
+		let setupfee = 0.00;
 		if(iSetupFee == 2) setupfee = 99.00;
 		if(iSetupFee == 3) setupfee = 199.00;
-		console.log(setupfee);
 
 		// Setup Fee Charge
 		Meteor.call('stripeCharge', token, setupfee, function(error, result){
-			if(error)
-			{
+			if (error) {
 				console.log(error);
-			}else{
-				console.log(result);
 			}
 		});
 		
 		// Stripe Subscription
-		var plan = $('#plans').find(":selected").val();
-		console.log(plan);
+		let plan = $('#plans').find(":selected").val();
 		
 		Meteor.call('createSubscription', token, subscriptionEmail, plan, function(error, customer){
-			if(error)
-			{
-			  console.log(error);
-			}
-			else
-			{
-			  console.log('Creating Customer success');
-			  console.log(customer);
-			  Router.go("/user_settings/instagram_accounts");
-			  
+			if (error) {
+				console.log(error);
+			} else {
+				console.log('Creating Customer success, updating Instagram collection');
+
+				// Instagram account update on successful payment
+				let instaId = Session.get("ig-id");
+				Instagrams.update({_id: instaId}, {
+					$set: {
+						active: true,
+						paid: true,
+						lastPayDate: Date.now()
+					}
+				}, function (err, res) {
+					if (err) {
+						console.log(err);
+					}
+					if (res) {
+						// TODO: change sweetAlert to Bert.alert()
+						sweetAlert("Setup fee and your Subscription is successfully charged!");
+						Router.go("/user_settings/instagram_accounts");
+					}
+				});
 			}
 		});
 	}
