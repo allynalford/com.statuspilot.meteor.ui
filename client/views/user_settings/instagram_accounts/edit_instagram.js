a75b412a0b308f1d6e52017d0c0474421fb77b4c
Anisuzzaman Khan <aniskhan001@outlook.com>
2017-10-15 13:39:45 +0600

    edit repeat_time

--- client/views/user_settings/instagram_accounts/edit_instagram.js
+++ client/views/user_settings/instagram_accounts/edit_instagram.js
@@ -56,40 +56,50 @@ Template.UserSettingsEditInstagram.events({
 		let features_like_hashtag = $(e.target).find('#like_hashtag').prop('checked');
 		let hashtags = t.find('#hashtags').value.trim();
 		let hashtags_amount = $(e.target).find('#hashtags_amount').val();
-
+		let repeat_like_hashtag = $(e.target).find('#repeat_like_hashtag').val();
+		
 		let features_like_medias_by_location = $(e.target).find('#like_medias_by_location').prop('checked');
 		let like_media_location = $(e.target).find('#like_media_location').val();
 		let like_media_location_amount = $(e.target).find('#like_media_location_amount').val();
+		let repeat_like_medias_by_location = $(e.target).find('#repeat_like_medias_by_location').val();
 
 		let features_like_timeline = $(e.target).find('#like_timeline').prop('checked');
 		let like_timeline_amount = $(e.target).find('#like_timeline_amount').val();
-
+		let repeat_like_timeline = $(e.target).find('#repeat_like_timeline').val();
+		
 		let features_follow_user_followers = $(e.target).find('#follow_user_followers').prop('checked');
 		let follow_users = $(e.target).find('#follow_users').val();
 		let follow_users_amount = $(e.target).find('#follow_users_amount').val();
-
+		let repeat_follow_user_followers = $(e.target).find('#repeat_follow_user_followers').val();
+		
 		let features_follow_by_location = $(e.target).find('#follow_by_location').prop('checked');
 		let follow_locations = $(e.target).find('#follow_locations').val();
 		let follow_locations_amount = $(e.target).find('#follow_locations_amount').val();
-
+		let repeat_follow_by_location = $(e.target).find('#repeat_follow_by_location').val();
+		
 		let features_follow_likers_by_location = $(e.target).find('#follow_likers_by_location').prop('checked');
 		let follow_location_likers = $(e.target).find('#follow_location_likers').val();
 		let follow_location_likers_amount = $(e.target).find('#follow_location_likers_amount').val();
-
+		let repeat_follow_likers_by_location = $(e.target).find('#repeat_follow_likers_by_location').val();
+		
 		let features_like_likers_by_location = $(e.target).find('#like_likers_by_location').prop('checked');
 		let like_location_likers = $(e.target).find('#like_location_likers').val();
 		let like_location_likers_amount = $(e.target).find('#like_location_likers_amount').val();
-
+		let repeat_like_likers_by_location = $(e.target).find('#repeat_like_likers_by_location').val();
+		
 		let features_direct_message_followers = $(e.target).find('#direct_message_followers').prop('checked');
 		let dm_followers = $(e.target).find('#dm_followers').val();
 		let dm_followers_message = $(e.target).find('#dm_followers_message').val();
 		let dm_followers_amount = $(e.target).find('#dm_followers_amount').val();
-
+		let repeat_direct_message_followers = $(e.target).find('#repeat_direct_message_followers').val();
+		
 		let features_direct_message_new_followers = $(e.target).find('#direct_message_new_followers').prop('checked');
 		let dm_new_followers_message = $(e.target).find('#dm_new_followers_message').val();
 		let dm_new_followers_amount = $(e.target).find('#dm_new_followers_amount').val();
-
+		let repeat_direct_message_new_followers = $(e.target).find('#repeat_direct_message_new_followers').val();
+		
 		let features_get_user_followers = $(e.target).find('#get_user_followers').prop('checked');
+		let repeat_get_user_followers = $(e.target).find('#repeat_get_user_followers').val();
 
 		let data = {
 			password : insta_pass,
@@ -104,6 +114,7 @@ Template.UserSettingsEditInstagram.events({
 						hashtags: hashtags.split(','),
 						amount: hashtags_amount
 					},
+					repeat_time: repeat_like_hashtag * 3600,
 					active: features_like_hashtag ? true : false
 				},
 				like_medias_by_location: {
@@ -111,12 +122,14 @@ Template.UserSettingsEditInstagram.events({
 						locations: like_media_location,
 						amount: like_media_location_amount
 					},
+					repeat_time: repeat_like_medias_by_location * 3600,
 					active: features_like_medias_by_location ? true : false
 				},
 				like_timeline: {
 					bot_params: {
 						amount: like_timeline_amount
 					},
+					repeat_time: repeat_like_timeline * 3600,
 					active: features_like_timeline ? true : false
 				},
 				follow_user_followers: {
@@ -124,6 +137,7 @@ Template.UserSettingsEditInstagram.events({
 						users: follow_users.split(','),
 						amount: follow_users_amount
 					},
+					repeat_time: repeat_follow_user_followers * 3600,
 					active: features_follow_user_followers ? true : false
 				},
 				follow_by_location: {
@@ -131,6 +145,7 @@ Template.UserSettingsEditInstagram.events({
 						locations: follow_locations.split(','),
 						amount: follow_locations_amount
 					},
+					repeat_time: repeat_follow_by_location * 3600,
 					active: features_follow_by_location ? true : false
 				},
 				follow_likers_by_location: {
@@ -138,6 +153,7 @@ Template.UserSettingsEditInstagram.events({
 						locations: follow_location_likers.split(','),
 						amount: follow_location_likers_amount
 					},
+					repeat_time: repeat_follow_likers_by_location * 3600,
 					active: features_follow_likers_by_location ? true : false
 				},
 				like_likers_by_location: {
@@ -145,6 +161,7 @@ Template.UserSettingsEditInstagram.events({
 						locations: like_location_likers.split(','),
 						amount: like_location_likers_amount
 					},
+					repeat_time: repeat_like_likers_by_location * 3600,
 					active: features_like_likers_by_location ? true : false
 				},
 				direct_message_followers: {
@@ -153,6 +170,7 @@ Template.UserSettingsEditInstagram.events({
 						users: dm_followers.split(','),
 						amount: dm_followers_amount
 					},
+					repeat_time: repeat_direct_message_followers * 3600,
 					active: features_direct_message_followers ? true : false
 				},
 				direct_message_new_followers: {
@@ -160,9 +178,11 @@ Template.UserSettingsEditInstagram.events({
 						message: dm_new_followers_message,
 						amount: dm_new_followers_amount
 					},
+					repeat_time: repeat_direct_message_new_followers * 3600,
 					active: features_direct_message_new_followers ? true : false
 				},
 				get_user_followers: {
+					repeat_time: repeat_get_user_followers * 3600,
 					active: features_get_user_followers ? true : false
 				}
 			}
